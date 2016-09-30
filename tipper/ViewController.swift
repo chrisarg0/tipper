044d215f2460d005be2acc2f57c520e536691928
Harsh Trivedi <htrivedi@elementumcom>
2016-09-29 18:30:15 -0700

    bring bill amount in focus on launch

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalField: UILabel!
+    
+    @IBOutlet weak var selectedTip: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //bring bill amount text field in focus
+        billTextField.becomeFirstResponder();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentages = [0.12, 0.15, 0.18, 0.20];
+        let billAmount = Double(billTextField.text!) ?? 0.00;
+        let tipAmount = billAmount * tipPercentages[selectedTip.selectedSegmentIndex];
+        
+        let totalAmount = billAmount + tipAmount;
+        
+        tipLabel.text = String(format: "%.2f", tipAmount);
+        totalField.text = String(format: "%.2f", totalAmount);
+    
+    }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true);
+    }
 
 }
 
